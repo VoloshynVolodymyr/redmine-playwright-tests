4fb8af3f4f941aa63ee665b60ef78bfe53762d8c
Volodymyr Voloshyn <mailvoloshyn@gmail.com>
2026-05-06 21:11:59 +0300

    fix: Remove baseURL from BasePage

--- pages/basePage.js
+++ pages/basePage.js
@@ -1,12 +1,10 @@
 export class BasePage {
   constructor(page) {
     this.page = page
-    this.baseUrl = process.env.PLAYWRIGHT_BASE_URL || 'https://www.redmine.org'
   }
 
   async navigate(path = '') {
-    const url = path.startsWith('http') ? path : `${this.baseUrl}${path}`
-    await this.page.goto(url)
+    await this.page.goto(path)
     await this.page.waitForLoadState('networkidle')
   }
 
